4e75192197fdf2acf12285ffeb0a06804a568f3a
Mehul-ui141 <harsh007mehul@gmail.com>
2026-03-16 12:07:03 +0530

    timepass2

--- src/test/test1.java
+++ src/test/test1.java
@@ -6,7 +6,6 @@ class test1 {
 
     static void main(String[] args) {
         Connection connection = null;
-
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
